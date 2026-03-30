07313c9b68dce495ad506672895e4884fac0eb7a
denalimarsh <denalimarsh@gmail.com>
2026-03-30 15:19:19 +0200

    extension types/version tests

--- test/integration/VersionedBeaconIntegration.t.sol
+++ test/integration/VersionedBeaconIntegration.t.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.34;
 
+import { IAccessControl } from "../../lib/evm-m-extensions/lib/common/lib/openzeppelin-contracts/contracts/access/IAccessControl.sol";
+
 import { IERC20 } from "../../lib/evm-m-extensions/lib/common/src/interfaces/IERC20.sol";
 
 import { IExtensionFactory } from "../../src/platform/interfaces/IExtensionFactory.sol";
+import { IVersionedBeacon } from "../../src/platform/interfaces/IVersionedBeacon.sol";
 import { YieldToOne } from "../../src/platform/projects/YieldToOne.sol";
 import { IntegrationForkTest } from "../utils/IntegrationForkTest.sol";
 
@@ -18,6 +21,7 @@ contract YieldToOneV2 is YieldToOne {
 
 contract VersionedBeaconIntegrationTests is IntegrationForkTest {
     bytes32 public constant YIELD_TO_ONE_TYPE_KEY = keccak256("YIELD_TO_ONE");
+    bytes32 public constant YIELD_SPLIT_TYPE_KEY = keccak256("YIELD_SPLIT");
 
     address public builder;
 
@@ -138,5 +142,141 @@ contract VersionedBeaconIntegrationTests is IntegrationForkTest {
 
         assertEq(extension.balanceOf(alice), 0);
         assertEq(IERC20(address(pyusdx)).balanceOf(alice), wrapAmount);
+    }
+
+    /* ============ Extension Type/Version Lifecycle ============ */
+
+    function test_newExtensionTypeRegistration() public {
+        // A brand new type key that the beacon has never seen
+        assertEq(beacon.versionCount(YIELD_SPLIT_TYPE_KEY), 0);
+
+        // Deploy a YieldToOne extension BEFORE registering the new type
+        vm.prank(admin);
+        uint256 ytoV1 = beacon.registerVersion(YIELD_TO_ONE_TYPE_KEY, yieldToOneImplV1);
+
+        IExtensionFactory.YieldToOneParams memory params = IExtensionFactory.YieldToOneParams({
+            name: "Existing Extension",
+            symbol: "EXT",
+            yieldRecipient: yieldRecipient,
+            admin: builder,
+            freezeManager: freezeManager,
+            pauser: pauser,
+            yieldRecipientManager: builder
+        });
+
+        vm.prank(builder);
+        address existingProxy = factory.deployBeaconYieldToOne("existing-ext", ytoV1, params);
+
+        // Wrap PYUSDX so we can verify balances are preserved
+        uint256 wrapAmount = 500e6;
+
+        vm.prank(earnerManager);
+        pyusdx.setAccountInfo(existingProxy, 500, 0, address(0));
+
+        _mintPYUSDX(alice, wrapAmount);
+
+        vm.prank(alice);
+        IERC20(address(pyusdx)).approve(address(swapFacility), wrapAmount);
+
+        vm.prank(alice);
+        swapFacility.swapIn(existingProxy, wrapAmount, alice);
+
+        assertEq(YieldToOne(existingProxy).balanceOf(alice), wrapAmount);
+
+        // Now register versions for the new YIELD_SPLIT type
+        vm.prank(admin);
+        uint256 v1 = beacon.registerVersion(YIELD_SPLIT_TYPE_KEY, yieldToOneImplV1);
+
+        vm.prank(admin);
+        uint256 v2 = beacon.registerVersion(YIELD_SPLIT_TYPE_KEY, yieldToOneImplV2);
+
+        // New type has correct state
+        assertEq(v1, 1);
+        assertEq(v2, 2);
+        assertEq(beacon.versionCount(YIELD_SPLIT_TYPE_KEY), 2);
+        assertEq(beacon.latestVersion(YIELD_SPLIT_TYPE_KEY), 1);
+        assertEq(beacon.getVersion(YIELD_SPLIT_TYPE_KEY, v1), yieldToOneImplV1);
+
+        // Version IDs are independent per type
+        assertEq(beacon.versionCount(YIELD_TO_ONE_TYPE_KEY), 1);
+        assertEq(beacon.versionCount(YIELD_SPLIT_TYPE_KEY), 2);
+
+        // Sanity check that a completely unknown type returns 0
+        assertEq(beacon.versionCount(keccak256("DOES_NOT_EXIST")), 0);
+
+        // Cross-type isolation: existing YieldToOne proxy is completely unaffected
+        assertEq(beacon.implementationFor(existingProxy), yieldToOneImplV1);
+        assertEq(beacon.proxyVersion(existingProxy), ytoV1);
+        assertEq(YieldToOne(existingProxy).balanceOf(alice), wrapAmount);
+
+        // Existing extension still works: alice can unwrap
+        vm.prank(alice);
+        IERC20(existingProxy).approve(address(swapFacility), wrapAmount);
+
+        vm.prank(alice);
+        swapFacility.swapOut(existingProxy, wrapAmount, alice);
+
+        assertEq(YieldToOne(existingProxy).balanceOf(alice), 0);
+    }
+
+    function test_registerVersion_revert_zeroTypeKey() public {
+        vm.prank(admin);
+        vm.expectRevert(IVersionedBeacon.InvalidTypeKey.selector);
+        beacon.registerVersion(bytes32(0), yieldToOneImplV1);
+    }
+
+    function test_pinVersion_revert_nonOwner() public {
+        // Setup: register version and deploy extension owned by builder
+        vm.prank(admin);
+        uint256 v1 = beacon.registerVersion(YIELD_TO_ONE_TYPE_KEY, yieldToOneImplV1);
+
+        IExtensionFactory.YieldToOneParams memory params = IExtensionFactory.YieldToOneParams({
+            name: "Builder YieldToOne",
+            symbol: "bYTO",
+            yieldRecipient: yieldRecipient,
+            admin: builder,
+            freezeManager: freezeManager,
+            pauser: pauser,
+            yieldRecipientManager: builder
+        });
+
+        vm.prank(builder);
+        address proxy = factory.deployBeaconYieldToOne("builder-pin-test", v1, params);
+
+        // Random user cannot pin
+        address randomUser = makeAddr("randomUser");
+
+        vm.prank(randomUser);
+        vm.expectRevert(IVersionedBeacon.NotProxyOwner.selector);
+        beacon.pinVersion(proxy, v1);
+
+        // Even M0 admin cannot force-upgrade a builder's extension
+        vm.prank(admin);
+        vm.expectRevert(IVersionedBeacon.NotProxyOwner.selector);
+        beacon.pinVersion(proxy, v1);
+    }
+
+    function test_pinVersion_revert_invalidVersion() public {
+        // Setup: register one version and deploy extension
+        vm.prank(admin);
+        uint256 v1 = beacon.registerVersion(YIELD_TO_ONE_TYPE_KEY, yieldToOneImplV1);
+
+        IExtensionFactory.YieldToOneParams memory params = IExtensionFactory.YieldToOneParams({
+            name: "Builder YieldToOne",
+            symbol: "bYTO",
+            yieldRecipient: yieldRecipient,
+            admin: builder,
+            freezeManager: freezeManager,
+            pauser: pauser,
+            yieldRecipientManager: builder
+        });
+
+        vm.prank(builder);
+        address proxy = factory.deployBeaconYieldToOne("builder-invalid-ver", v1, params);
+
+        // Builder tries to pin to a version that doesn't exist
+        vm.prank(builder);
+        vm.expectRevert(IVersionedBeacon.InvalidVersion.selector);
+        beacon.pinVersion(proxy, 99);
     }
 }
